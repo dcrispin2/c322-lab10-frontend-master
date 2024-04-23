e0f83a7d9ab1862f11d8f5404167fdf02e38f44c
dcrispin <dcrispin@iu.edu>
2024-04-22 23:10:57 -0400

    Initial commit

---
+++ configuration.js
@@ -0,0 +1,10 @@
+const mode = 1;
+
+const host_local = "http://localhost:8080";
+const host_remote = let host = "https://ducks-service-latest-1-m472.onrender.com";;
+
+function getHost() {
+    return (mode == 0) ? host_local : host_remote;
+}
+
+let configuration = {loggedIn: false, hosts: getHost(), token: ""};
